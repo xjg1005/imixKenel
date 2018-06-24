42e7fb09bae02ee1f2a6a85c785d46f32a5091fc
xiangjunguo <xjg_1005@163.com>
2018-06-24 08:16:01 +0800

    编译通过&&添加.gitignor文件

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Jun 24 07:45:47 CST 2018"
+#define LINUX_COMPILE_BY "xjg"
+#define LINUX_COMPILE_HOST "xjg-pc"
+#define LINUX_COMPILER "gcc version 5.3.1 20160113 (Linaro GCC 5.3-2016.02) "
